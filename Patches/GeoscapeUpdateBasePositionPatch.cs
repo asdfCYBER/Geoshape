c2fa57c778c3e0646025bf13d04910cb60729b50
asdfCYBER <asdfCYBER@outlook.com>
2025-04-24 00:00:29 +0200

    Start exploring radar range visualisation

---
+++ Patches/GeoscapeUpdateBasePositionPatch.cs
@@ -0,0 +1,21 @@
+﻿using Geoshape.Logging;
+using HarmonyLib;
+using UnityEngine;
+using Xenonauts.Strategy.UI;
+
+
+namespace Geoshape.Patches
+{
+    [HarmonyPatch(typeof(GeoscapeBaseBuildElement), nameof(GeoscapeBaseBuildElement.UpdateBaseIconPosition))]
+    internal class GeoscapeUpdateBasePositionPatch
+    {
+        private static void Postfix(GeoscapeBaseBuildElement __instance, ref GameObject ____radarRootInWorldPos)
+        {
+            Debug.Log("UpdateBaseIconPosition patch called. Dump of instance:");
+            __instance.gameObject.DumpToLog();
+
+            Debug.Log("Dump of _radarRootInWorldPos:");
+            ____radarRootInWorldPos.DumpToLog();
+        }
+    }
+}
